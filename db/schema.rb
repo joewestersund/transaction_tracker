3109ae314b53f840874dcd218a98419821756b42
Joe Westersund <joe_westersund@hotmail.com>
2013-12-25 16:09:25 -0800

    add pw and remember token

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131224074640) do
+ActiveRecord::Schema.define(version: 20131226000815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,9 @@ ActiveRecord::Schema.define(version: 20131224074640) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
+    t.string   "password_digest"
+    t.string   "remember_token"
   end
 
 end
