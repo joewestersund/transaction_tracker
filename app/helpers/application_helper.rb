e1dbe0b0ec4987a5b95796b8ec627b01d4d14356
Joe Westersund <joe_westersund@hotmail.com>
2013-12-31 22:41:30 -0800

    change Transaction Tracker to Spending Tracker

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title(page_title)
-    base_title = "Transaction Tracker"
+    base_title = "Spending Tracker"
     if(page_title.empty?)
       base_title
     else
