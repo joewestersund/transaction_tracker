629f9dabda95f1d024958b8694d91a2e25f1dc78
Joe Westersund <joe_westersund@hotmail.com>
2018-01-14 10:45:10 -0800

    add index for transactions/index page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140101231640) do
+ActiveRecord::Schema.define(version: 20180114183705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20140101231640) do
 
   add_index "transactions", ["account_id"], name: "index_transactions_on_account_id", using: :btree
   add_index "transactions", ["transaction_category_id"], name: "index_transactions_on_transaction_category_id", using: :btree
+  add_index "transactions", ["user_id", "transaction_date"], name: "index_transactions_on_user_id_and_transaction_date", order: {"transaction_date"=>:desc}, using: :btree
 
   create_table "transfers", force: :cascade do |t|
     t.integer  "user_id"
