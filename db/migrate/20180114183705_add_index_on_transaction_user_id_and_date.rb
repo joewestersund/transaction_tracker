143deaf24dcd4e8ff2025f31cf5de07871f3058e
Joe Westersund <joe_westersund@hotmail.com>
2019-11-07 23:40:22 -0800

    specify ActiveRecord::Migration number in migrations that didn't have it

--- db/migrate/20180114183705_add_index_on_transaction_user_id_and_date.rb
+++ db/migrate/20180114183705_add_index_on_transaction_user_id_and_date.rb
@@ -1,4 +1,4 @@
-class AddIndexOnTransactionUserIdAndDate < ActiveRecord::Migration
+class AddIndexOnTransactionUserIdAndDate < ActiveRecord::Migration[5.1]
   def change
     add_index :transactions, [:user_id, :transaction_date], order: {user_id: :asc, transaction_date: :desc}
   end
--- db/migrate/20180114184714_add_index_on_transfer_user_id_and_date.rb
+++ db/migrate/20180114184714_add_index_on_transfer_user_id_and_date.rb
@@ -1,4 +1,4 @@
-class AddIndexOnTransferUserIdAndDate < ActiveRecord::Migration
+class AddIndexOnTransferUserIdAndDate < ActiveRecord::Migration[5.1]
   def change
     add_index :transfers, [:user_id, :transfer_date], order: {user_id: :asc, transfer_date: :desc}
   end
